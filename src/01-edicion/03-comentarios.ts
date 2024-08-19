abfc6e126915f1b617302674c19c55e59d292929
Jose Angel <ah@xdevelop.mx>
2024-08-18 18:26:22 -0600

    comentar lines

--- src/01-edicion/03-comentarios.ts
+++ src/01-edicion/03-comentarios.ts
@@ -6,11 +6,12 @@
 //*        ⌘ /
 //*        
 //*        Ctrl + \
+//*   Tip es Ctrl + }
 //*
 
-const a = 10;
-const b = 20;
-const c = { a, b };
+// const a = 10;
+// const b = 20;
+// const c = { a, b };
 
 
 const a = 30;
